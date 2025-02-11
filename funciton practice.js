dafc2759913906300436dc8ac2756223dd8b7eea
Ammar Shahab <ammar.qa@generalpharma.com>
2025-02-11 14:17:01 +0600

    Task 1 completed

--- funciton practice.js
+++ funciton practice.js
@@ -31,3 +31,33 @@ function cToF(param) {
 
 let temInC = cToF(22);
 console.log(`Today's temperature is ${temInC}°F.`);
+
+/* 
+!Task-2:
+You are given an array of numbers. Count how many times the a number is repeated in the array.
+
+sample-input: numbers = [5,6,11,12,98, 5]
+find: 5
+output: 2
+
+sample-input:
+numbers = [5,6,11,12,98, 5]
+find: 25
+output: 0
+ */
+
+let numbers = [5, 6, 11, 12, 98, 5];
+
+function countRepeat(arr) {
+  let dupArr = [];
+
+  for (const number of arr) {
+    if (dupArr.includes(number) === false) {
+      dupArr.push(number);
+    }
+  }
+  return arr.length - dupArr.length;
+}
+
+let arrOfNum = countRepeat(numbers);
+console.log(arrOfNum);
